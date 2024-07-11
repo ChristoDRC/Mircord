ed297f0a0a7be953707c9e80eafb0b063ec32dd2
Christopher Daniel Ramon Chable <christodrc@gmail.com>
2024-07-11 17:04:36 -0500

    Se edita el navbar
    Se edita el navbar

--- JavaScript/Javascript.js
+++ JavaScript/Javascript.js
@@ -7,16 +7,3 @@ buttonMenu.addEventListener('click', (e) => {
   navMenu.classList.toggle('open-menu');
 });
 
-// jQuery Version
-// $(function() {
-//     var btn_movil = $(‘#nav-mobile’),
-//     menu = $(‘#menu’).find(‘ul’);
-
-//     // Al dar click agregar/quitar clases que permiten el despliegue del menú
-//     btn_movil.on(‘click’, function (e) {
-//         e.preventDefault();
-//         var el = $(this);
-//         el.toggleClass(‘nav-active’);
-//         menu.toggleClass(‘open-menu’);
-//     });
-// });

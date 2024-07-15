d8512c8c102f01fa313ca31daa7d34baca8b1ff6
Christopher Daniel Ramon Chable <christodrc@gmail.com>
2024-07-15 16:34:57 -0500

    Se re hizo el buscador

--- JavaScript/Javascript.js
+++ JavaScript/Javascript.js
@@ -1,9 +1,15 @@
-// JavaScript Version
-const buttonMenu = document.querySelector('#nav-mobile');
-const navMenu = document.querySelector('.nav-menu');
-
-buttonMenu.addEventListener('click', (e) => {
-  e.currentTarget.classList.toggle('nav-open');
-  navMenu.classList.toggle('open-menu');
+document.addEventListener("DOMContentLoaded", function() {
+  var dropdowns = document.querySelectorAll('.dropdown-toggle');
+  
+  dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('click', function(e) {
+      e.preventDefault();
+      var content = this.nextElementSibling;
+      if (content.classList.contains('show')) {
+        content.classList.remove('show');
+      } else {
+        content.classList.add('show');
+      }
+    });
+  });
 });
-

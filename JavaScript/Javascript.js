7da841febb2c6357f3aaa35586c3cc638d5b50e4
Christopher Daniel Ramon Chable <christodrc@gmail.com>
2024-07-17 17:42:27 -0500

    Animaciónes del footer
    Se incluyen las animaciónes del footer

--- JavaScript/Javascript.js
+++ JavaScript/Javascript.js
@@ -1,25 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const navMobile = document.getElementById('nav-mobile');
+  const navMenu = document.querySelector('.nav-menu');
   const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
   
-  dropdownToggles.forEach(toggle => {
-      const dropdownMenu = toggle.nextElementSibling;
-      
-      toggle.addEventListener('click', function (event) {
-          const isVisible = dropdownMenu.style.display === 'block';
-          dropdownMenu.style.display = isVisible ? 'none' : 'block';
-          toggle.querySelector('svg').style.transform = isVisible ? 'rotate(0deg)' : 'rotate(180deg)';
-          event.stopPropagation();
-      });
+  navMobile.addEventListener('click', function () {
+    navMenu.classList.toggle('open');
   });
 
-  // Cierra los menús si se hace clic fuera de ellos
-  window.addEventListener('click', function (e) {
-      dropdownToggles.forEach(toggle => {
-          const dropdownMenu = toggle.nextElementSibling;
-          if (!toggle.contains(e.target)) {
-              dropdownMenu.style.display = 'none';
-              toggle.querySelector('svg').style.transform = 'rotate(0deg)';
-          }
+  dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('click', function (event) {
+      const dropdownMenu = toggle.nextElementSibling;
+      const isVisible = dropdownMenu.style.display === 'block';
+
+      // Close all dropdowns
+      dropdownToggles.forEach(otherToggle => {
+        const otherDropdownMenu = otherToggle.nextElementSibling;
+        if (otherDropdownMenu !== dropdownMenu) {
+          otherDropdownMenu.style.display = 'none';
+          otherToggle.querySelector('svg').style.transform = 'rotate(0deg)';
+        }
       });
+
+      // Toggle the clicked dropdown
+      dropdownMenu.style.display = isVisible ? 'none' : 'block';
+      toggle.querySelector('svg').style.transform = isVisible ? 'rotate(0deg)' : 'rotate(180deg)';
+
+      event.stopPropagation();
+    });
+  });
+
+  // Close all dropdowns if clicking outside
+  window.addEventListener('click', function () {
+    dropdownToggles.forEach(toggle => {
+      const dropdownMenu = toggle.nextElementSibling;
+      dropdownMenu.style.display = 'none';
+      toggle.querySelector('svg').style.transform = 'rotate(0deg)';
+    });
   });
 });
